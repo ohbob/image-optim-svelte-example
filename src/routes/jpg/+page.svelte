f9e60d69abe07fae1a397aa8ee4c41a6330c99a8
Camo <roberts@nallus.com>
2023-01-18 13:50:07 +0200

    up
    updt

--- src/routes/jpg/+page.svelte
+++ src/routes/jpg/+page.svelte
@@ -1,20 +1,17 @@
-
-
-
-
 <script>
-  import Image from "$lib/components/Image.svelte";
-  import Title from "$lib/components/Title.svelte";
+import Image from "$lib/components/Image.svelte";
+import Title from "$lib/components/Title.svelte";
+const FORMATS = ['jpg']
 </script>
 
 <main class="container mx-auto pb-10">
-  <section style="height:400px">
-    <Image FORMATS="webp" src="hero.jpg" />
-  </section>
-  <Title content="JPG" />
-  <section class="grid md:grid-cols-3 lg:grid-cols-4 gap-x-5 md:gap-y-7">
-    {#each [...Array(16).keys()] as n}
-      <Image FORMATS="jpg" src="{n}.jpg" />
-    {/each}
-  </section>
+    <section style="height:400px">
+        <Image FFORMATS={FORMATS} src="hero.jpg" />
+    </section>
+    <Title content="JPG" />
+    <section class="grid md:grid-cols-3 lg:grid-cols-4 gap-x-5 md:gap-y-7">
+        {#each [...Array(16).keys()] as n}
+        <Image FORMATS={FORMATS} src="{n}.jpg" />
+        {/each}
+    </section>
 </main>
